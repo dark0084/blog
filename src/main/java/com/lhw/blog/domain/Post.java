ceaeae403631dfaeacd9375a397e3f85c1a158d1
null0584 <null0584@sesisoft.com>
2019-03-13 15:15:42 +0900

    Post 클래스에 date 추가

--- src/main/java/com/lhw/blog/domain/Post.java
+++ src/main/java/com/lhw/blog/domain/Post.java
@@ -7,6 +7,10 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import java.util.Date;
 
 @Data
 @ToString
@@ -19,4 +23,7 @@ public class Post {
     private String id;
     private String title;
     private String content;
+
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+	private Date date;
 }
